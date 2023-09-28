88425e68f82c0ee36c404cc2c39d83e10a6bfd4e
yunfei6688 <119996269+yunfei6688@users.noreply.github.com>
2023-09-28 17:46:06 +0800

    Add files via upload

---
+++ ytkj1-main/config/index.ts
@@ -0,0 +1,17 @@
+import type { AppInfo } from '@/types/app'
+export const APP_ID = `6b32f3a6-327e-43c4-a17e-432250b34e99`
+export const API_KEY = `app-psETgVKzohYGLzIEbTjiIQud`
+export const API_URL = `https://api.dify.ai/v1`
+export const APP_INFO: AppInfo = {
+  title: '标题生成器',
+  description: 'AI一键生成标题',
+  copyright: '@盈腾科技',
+  privacy_policy: '',
+  default_language: 'zh-Hans',
+}
+
+export const API_PREFIX = '/api'
+
+export const LOCALE_COOKIE_NAME = 'locale'
+
+export const DEFAULT_VALUE_MAX_LEN = 48
